49342b0bbb99e6cec1e33eef9583a1cbe5dd1511
Inaboya <momohdavid211@gmail.com>
2023-09-02 17:43:23 +0100

    set up project

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
+import { TodosModule } from './todos/todos.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, TodosModule],
   controllers: [AppController, UsersController],
   providers: [AppService],
 })
